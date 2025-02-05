f99eaaaba741c2fd911d9efc1f39dbb2ae04b7ec
“Stanislav <lacost.st@gmail.com>
2025-02-05 20:04:41 +0200

    chore: links updated to target blank

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,18 +7,24 @@ export default function Footer() {
         <p>&copy; 2025 Valu iFrame Sample App</p>
         <div className="space-x-4">
 
-          <a href="https://github.com/Roomful/valu-api" className="text-blue-600 hover:underline">
+          <a href="https://github.com/Roomful/valu-api"
+             target="_blank"
+             className="text-blue-600 hover:underline">
             GitHub
           </a>
-          <a href="https://www.npmjs.com/package/@arkeytyp/valu-api" className="text-blue-600 hover:underline">
+          <a href="https://www.npmjs.com/package/@arkeytyp/valu-api"
+             target="_blank"
+             className="text-blue-600 hover:underline">
             NPM Package
           </a>
           <a href="https://github.com/Roomful/valu-api?tab=readme-ov-file#install"
+             target="_blank"
              className="text-blue-600 hover:underline">
             Documentation
           </a>
 
           <a href="https://github.com/Roomful/valusocial-sample-app"
+             target="_blank"
              className="text-blue-600 hover:underline">
             Application Code
           </a>
